3b03a18ab7b3118e6a5d0ce9bc7c1752cd44741d
Gagandeep Guru <85410287+igagandeep@users.noreply.github.com>
2025-10-17 15:15:53 -0400

    Time: 12 ms (21.74%), Space: 65 MB (38.98%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+   
+    return x.toString().split('').join('') === x.toString().split('').reverse().join('');
+};
